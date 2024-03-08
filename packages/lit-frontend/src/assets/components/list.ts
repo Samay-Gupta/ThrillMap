a891e11b2b8ed7c50f6d9986e7ad6306e94e3d00
Samay Gupta <samay13g@icoud.com>
2024-03-08 02:20:31 -0800

    lint

--- packages/lit-frontend/src/assets/components/list.ts
+++ packages/lit-frontend/src/assets/components/list.ts
@@ -91,108 +91,110 @@ const listTemplate = (
 };
 
 const listStyles = css`
-    .list-title {
-        font-size: 2em;
-        color: var(--color-gray2);
-        margin-bottom: 20px;
-        text-align: center;
-        font-weight: bold;
-    }
+  .list-title {
+    font-size: 2em;
+    color: var(--color-gray2);
+    margin-bottom: 20px;
+    text-align: center;
+    font-weight: bold;
+  }
 
-    .list {
-        width: 80vw;
-        margin: auto;
-        padding: 15px;
-    }
+  .list {
+    width: 80vw;
+    margin: auto;
+    padding: 15px;
+  }
 
-    .list-info {
-        display: flex;
-        align-items: center;
-        margin-bottom: 20px;
-        padding: 10px;
-        border: 1px solid var(color-gray1);
-        border-radius: 8px;
-        background-color: var(--color-background-secondary);
-        text-decoration: none;
-        color: var(--color-gray2);
-        box-shadow: 0 2px 4px var(--color-shadow1);
-        transition: box-shadow 0.3s ease, transform 0.2s ease;
-    }
+  .list-info {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 1px solid var(color-gray1);
+    border-radius: 8px;
+    background-color: var(--color-background-secondary);
+    text-decoration: none;
+    color: var(--color-gray2);
+    box-shadow: 0 2px 4px var(--color-shadow1);
+    transition:
+      box-shadow 0.3s ease,
+      transform 0.2s ease;
+  }
 
-    .list-image {
-        width: 150px;
-        height: auto;
-        object-fit: cover;
-        margin-right: 15px;
-        border-radius: 5px;
-    }
+  .list-image {
+    width: 150px;
+    height: auto;
+    object-fit: cover;
+    margin-right: 15px;
+    border-radius: 5px;
+  }
 
-    .list-container {
-        display: flex;
-        flex-direction: column;
-    }
+  .list-container {
+    display: flex;
+    flex-direction: column;
+  }
 
-    .list-info h3 {
-        margin-bottom: 10px;
-    }
+  .list-info h3 {
+    margin-bottom: 10px;
+  }
 
-    .list-info p {
-        color: #666;
-    }
+  .list-info p {
+    color: #666;
+  }
 
-    .list-info:hover {
-        box-shadow: var(--box-shadow2);
-        transform: var(--transition-hover);
-    }
+  .list-info:hover {
+    box-shadow: var(--box-shadow2);
+    transform: var(--transition-hover);
+  }
 
-    .list-filter {
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        padding: 10px;
-        border-radius: 5px;
-        margin-bottom: 20px;
-        background-color: var(--color-background-secondary);
-        box-shadow: 0 2px 4px var(--color-shadow1);
-    }
+  .list-filter {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    background-color: var(--color-background-secondary);
+    box-shadow: 0 2px 4px var(--color-shadow1);
+  }
 
-    .input-filter {
-        font-size: 1.2em;
-        min-width: 30vw;
-        padding: 5px 10px;
-        border: 1px solid var(--color-gray1);
-        border-radius: 10px;
-        outline: none;
-        background-color: var(--color-background-primary);
-    }
+  .input-filter {
+    font-size: 1.2em;
+    min-width: 30vw;
+    padding: 5px 10px;
+    border: 1px solid var(--color-gray1);
+    border-radius: 10px;
+    outline: none;
+    background-color: var(--color-background-primary);
+  }
 
-    .input-filter:focus {
-        border-color: var(--color-primary);
-    }
-    
-    .select-filter {
-        padding: 8px 24px 8px 12px;
-        border: 1px solid var(--color-gray1);
-        border-radius: 10px;
-        outline: none;
-        min-width: 5vw;
-        appearance: none;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        background-color: color(--color-secondary);
-        cursor: pointer;
-        font-size: 1rem;
-        color: var(--color-gray2);
-        background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="%23333" d="M6.5,7.5l3.5,3.5l3.5,-3.5l2.5,2.5l-6,6l-6,-6l2.5,-2.5Z"/></svg>');
-        background-repeat: no-repeat;
-        background-position: right 10px center;
-        background-size: 12px 12px;
-        background-color: var(--color-background-primary);
-    }
-    
-    .select-filter:focus {
-      border-color: var(--color-primary);
-    }
+  .input-filter:focus {
+    border-color: var(--color-primary);
+  }
+
+  .select-filter {
+    padding: 8px 24px 8px 12px;
+    border: 1px solid var(--color-gray1);
+    border-radius: 10px;
+    outline: none;
+    min-width: 5vw;
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background-color: color(--color-secondary);
+    cursor: pointer;
+    font-size: 1rem;
+    color: var(--color-gray2);
+    background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="%23333" d="M6.5,7.5l3.5,3.5l3.5,-3.5l2.5,2.5l-6,6l-6,-6l2.5,-2.5Z"/></svg>');
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+    background-size: 12px 12px;
+    background-color: var(--color-background-primary);
+  }
+
+  .select-filter:focus {
+    border-color: var(--color-primary);
+  }
 `;
 
 @customElement('app-list')
@@ -216,7 +218,10 @@ class List extends LitElement {
 
   updateFilter(e: Event) {
     const target = e.target as HTMLInputElement | HTMLSelectElement;
-    const value = target.type === 'checkbox' ? (target as HTMLInputElement).checked : target.value;
+    const value =
+      target.type === 'checkbox'
+        ? (target as HTMLInputElement).checked
+        : target.value;
     const name = target.name;
     this.filter = { ...this.filter, [name]: value };
     this.requestUpdate();
