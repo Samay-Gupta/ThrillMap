891b900b6c734f40cda8b60379f24350bed698cc
Samay Gupta <samay13g@icoud.com>
2024-03-08 03:00:14 -0800

    Fix

--- packages/proto/static/js/rides/ride_list.js
+++
@@ -1,4 +0,0 @@
-function createRideElement(ride) {
-    const rideUrl = `/rides/ride_details.html?ride=${encodeURIComponent(ride.name)}`;
-    return createListElement(rideUrl, image(ride.imageURL), ride.name, ride.description);
-}
