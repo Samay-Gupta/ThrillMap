c832d1da2911b98700447d882c346f6f6d094426
Samay Gupta <samay13g@icoud.com>
2024-03-12 23:58:59 -0700

    env fix

--- packages/lit-frontend/src/config/constants.ts
+++ packages/lit-frontend/src/config/constants.ts
@@ -1,4 +1,8 @@
-export const API_ROOT = process.env.THRILL_MAP_API_ROOT || 'http://localhost:3000/api';
+const THRILL_MAP_API_ROOT = import.meta.env.VITE_THRILL_MAP_API_ROOT;
+
+console.log('THRILL_MAP_API_ROOT', THRILL_MAP_API_ROOT);
+
+export const API_ROOT = THRILL_MAP_API_ROOT || 'http://localhost:3000/api';
 
 export const AUTH_TOKEN_KEY = 'JWT_AUTH_TOKEN';
 
