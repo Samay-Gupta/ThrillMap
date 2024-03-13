e0ed85f8750aa9ac475ee51ec7ed0574ddfcdc98
Samay Gupta <samay13g@icoud.com>
2024-03-13 00:01:48 -0700

    Hardcode api url

--- packages/lit-frontend/src/config/constants.ts
+++ packages/lit-frontend/src/config/constants.ts
@@ -1,8 +1,4 @@
-const THRILL_MAP_API_ROOT = import.meta.env.VITE_THRILL_MAP_API_ROOT;
-
-console.log('THRILL_MAP_API_ROOT', THRILL_MAP_API_ROOT);
-
-export const API_ROOT = THRILL_MAP_API_ROOT || 'http://localhost:3000/api';
+export const API_ROOT = 'https://thrill-map-api.netlify.app/api';
 
 export const AUTH_TOKEN_KEY = 'JWT_AUTH_TOKEN';
 
